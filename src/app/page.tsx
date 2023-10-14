6141c415dc205cefb30022d43c5b976d1ad68d93
sobhanessi <sobhan@esfandyari.com>
2023-10-14 12:06:06 +0200

    first version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,11 @@ import { styled, useTheme } from '@mui/material/styles';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../public/logo.png';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+import { Young_Serif } from 'next/font/google';
+
+const youngSerif = Young_Serif({ subsets: ['latin'], weight: '400' });
 
 const drawerWidth = 240;
 
@@ -111,160 +116,274 @@ export default function Navbar(): JSX.Element {
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
+    <React.Fragment>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
 
-      <AppBar position="fixed" open={open} sx={{ backgroundColor: BG_COLOR }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              mr: 2,
-              display: NAVBAR_ICON_BUTTON_DISPLAY,
-            }}
-          >
-            <MenuIcon
+        <AppBar position="fixed" open={open} sx={{ backgroundColor: BG_COLOR }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
               sx={{
+                mr: 2,
                 display: NAVBAR_ICON_BUTTON_DISPLAY,
               }}
-            />
-          </IconButton>
-          <Container
-            sx={{
-              display: NAVBAR_CONTAINER_DISPLAY,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}
-            maxWidth="xl"
-          >
-            <Grid container display="flex" justifyContent="center">
-              <Grid
-                item
-                xl={2}
-                lg={2}
-                md={2}
-                sm={2}
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Image src={logo} alt="golden touchstories logo" />
-              </Grid>
-
-              <Grid
-                item
-                display="flex"
-                alignItems="center"
-                flexDirection="column"
-                xl={10}
-                lg={10}
-                md={10}
-                sm={10}
-              >
-                <Typography display="flex" textAlign="center" mt={'3%'}>
-                  Golden Touchstories
-                </Typography>
+            >
+              <MenuIcon
+                sx={{
+                  display: NAVBAR_ICON_BUTTON_DISPLAY,
+                }}
+              />
+            </IconButton>
+            <Container
+              sx={{
+                display: NAVBAR_CONTAINER_DISPLAY,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+              maxWidth="xl"
+            >
+              <Grid container display="flex" justifyContent="center">
                 <Grid
                   item
-                  container
+                  xl={2}
+                  lg={2}
+                  md={2}
+                  sm={2}
                   display="flex"
                   justifyContent="center"
                   alignItems="center"
-                  textAlign="center"
+                >
+                  <Image src={logo} alt="golden touchstories logo" />
+                </Grid>
+
+                <Grid
+                  item
+                  display="flex"
+                  alignItems="center"
+                  flexDirection="column"
                   xl={10}
                   lg={10}
                   md={10}
                   sm={10}
-                  pb={'2%'}
                 >
-                  {navigation.map((title) => (
-                    <Link
-                      href={title.href}
-                      key={title.name}
-                      style={{
-                        ...NAVBAR_APPBAR_TYPOGRAPHY,
-                        color: WHITE_COLOR,
-                        textDecoration: 'none',
-                        marginTop: 10,
-                        margin: '0 10px', // Adjust margin as needed
-                      }}
-                      className="times"
-                    >
-                      {title.name}
+                  <Typography
+                    display="flex"
+                    textAlign="center"
+                    mt={'3%'}
+                    className={youngSerif.className}
+                    fontWeight="bold"
+                    fontSize="1.75rem"
+                  >
+                    Golden Touchstories
+                  </Typography>
+                  <Grid
+                    item
+                    container
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    textAlign="center"
+                    xl={10}
+                    lg={10}
+                    md={10}
+                    sm={10}
+                    pb={'2%'}
+                  >
+                    {navigation.map((title) => (
+                      <Link
+                        href={title.href}
+                        key={title.name}
+                        style={{
+                          ...NAVBAR_APPBAR_TYPOGRAPHY,
+                          color: WHITE_COLOR,
+                          textDecoration: 'none',
+                          marginTop: 10,
+                          margin: '0 10px', // Adjust margin as needed
+                        }}
+                        // className="times"
+                        className="font-serif"
+                      >
+                        {title.name}
+                      </Link>
+                    ))}
+                    <Link href="https://www.instagram.com/goldentouchstories/?utm_source=ig_web_button_share_sheet&igshid=ODE2OTA4Y2Y1MQ==">
+                      <InstagramIcon sx={{ color: 'white' }} />
                     </Link>
-                  ))}
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </Container>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          backgroundColor: MAIN_COLOR,
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-          display: NAVBAR_ICON_BUTTON_DISPLAY,
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader sx={{ backgroundColor: MAIN_COLOR }}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? (
-              <ChevronLeftIcon sx={{ color: 'white' }} />
-            ) : (
-              <ChevronRightIcon sx={{ color: 'white' }} />
-            )}
-          </IconButton>
-        </DrawerHeader>
-
-        <Divider />
-
-        <List
+            </Container>
+          </Toolbar>
+        </AppBar>
+        <Drawer
           sx={{
             backgroundColor: MAIN_COLOR,
-            height: '100%',
-            pl: 1,
-            pt: 3,
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+            display: NAVBAR_ICON_BUTTON_DISPLAY,
           }}
+          variant="persistent"
+          anchor="left"
+          open={open}
         >
-          {/* <Grid container>
+          <DrawerHeader sx={{ backgroundColor: MAIN_COLOR }}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? (
+                <ChevronLeftIcon sx={{ color: 'white' }} />
+              ) : (
+                <ChevronRightIcon sx={{ color: 'white' }} />
+              )}
+            </IconButton>
+          </DrawerHeader>
+
+          <Divider />
+
+          <List
+            sx={{
+              backgroundColor: MAIN_COLOR,
+              height: '100%',
+              pl: 1,
+              pt: 3,
+            }}
+          >
+            {/* <Grid container>
             <Grid item display="flex" sm={2} xs={6} mb="5%">
-              <Image src={logo} alt="golden touchstories logo" />
+            <Image src={logo} alt="golden touchstories logo" />
             </Grid>
           </Grid> */}
 
-          {navigation.map((title) => (
-            <Link
-              href={title.href}
-              key={title.name}
-              style={{
-                ...NAVBAR_APPBAR_TYPOGRAPHY,
-                color: WHITE_COLOR,
-                textDecoration: 'none',
-                paddingBottom: '3%',
-                overflow: 'visible',
-              }}
-              className="times"
-            >
-              {title.name}
-            </Link>
-          ))}
-        </List>
-        <Divider />
-      </Drawer>
-      {/* <Main open={open}>
+            {navigation.map((title) => (
+              <Link
+                href={title.href}
+                key={title.name}
+                style={{
+                  ...NAVBAR_APPBAR_TYPOGRAPHY,
+                  color: WHITE_COLOR,
+                  textDecoration: 'none',
+                  paddingBottom: '3%',
+                  overflow: 'visible',
+                }}
+                className="times"
+              >
+                {title.name}
+              </Link>
+            ))}
+          </List>
+          <Divider />
+        </Drawer>
+        {/* <Main open={open}>
         <DrawerHeader />
       </Main> */}
-    </Box>
+      </Box>
+      <Container
+        sx={{
+          pt: '15%',
+        }}
+      >
+        <Box>
+          <Grid container>
+            <Grid
+              item
+              maxWidth="lg"
+              lg={8}
+              md={8}
+              sm={10}
+              xs={10}
+              className="mx-auto"
+            >
+              <Typography className="calibri" fontSize="1.4rem">
+                ANALY GOLDENTOUCH & SEYEDALI PEYGHAMBARI
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              maxWidth="lg"
+              lg={8}
+              md={8}
+              xs={10}
+              sm={10}
+              className="mx-auto"
+            >
+              <Typography
+                className="calibri"
+                gutterBottom
+                fontSize="1.3rem"
+                mb={5}
+              >
+                Screenwriter | Children\'s Literature Author | Lyricist |
+                Psychologist
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid
+              item
+              maxWidth="lg"
+              lg={8}
+              md={8}
+              sm={10}
+              xs={10}
+              className="mx-auto"
+            >
+              <Typography className="calibri" gutterBottom fontSize="1.15rem">
+                Analy and Ali, a couple deeply passionate about storytelling,
+                find joy in the art of creation. They've discovered their talent
+                in crafting captivating screenplays, weaving enchanting tales
+                for young readers, and crafting lyrics that touch the heart.
+                Together, they've nurtured a harmonious creative partnership,
+                bound by their dedication to the craft of breathing life into
+                stories using the power of words, scripts, and melodies. Their
+                journey in the realm of storytelling is a reflection of their
+                deep love for the art, a journey they continue to hold dear.
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              maxWidth="lg"
+              lg={8}
+              md={8}
+              sm={10}
+              xs={10}
+              className="mx-auto"
+            >
+              <Typography className="calibri" gutterBottom fontSize="1.15rem">
+                Prior to their journey as storytellers, Analy and Ali also held
+                careers as psychologists, where they gained insights into the
+                human psyche. However, their shared passion for the creative
+                arts eventually led them to shift their focus. They made the
+                courageous decision to follow their hearts and devote themselves
+                to the world of storytelling, a journey they continue to hold
+                dear.
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              maxWidth="lg"
+              lg={8}
+              md={8}
+              sm={10}
+              xs={10}
+              className="mx-auto"
+            >
+              <Typography className="calibri" gutterBottom fontSize="1.15rem">
+                Their experiences as psychologists have enriched their
+                storytelling, infusing depth and understanding into their
+                characters and narratives. Their love for the art is a guiding
+                force, and their journey is an evolving tale of dedication to
+                the craft and the pursuit of creative excellence.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
